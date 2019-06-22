acf772ccf318d5ec30ba686d932b97ea6a022fa5
maclyn02 <mac.afonso@yahoo.com>
2019-06-22 16:27:26 +0100

    First commit

--- src/main/java/AutomationPractice/Utils/AutomationConstants.java
+++ src/main/java/AutomationPractice/Utils/AutomationConstants.java
@@ -5,6 +5,9 @@ public class AutomationConstants {
     //Global Test Data
     public static final String baseURL = "http://automationpractice.com";
     //Browser settings
-    public static final String BROWSER_TYPE = "Chrome";
+    public static final String BROWSER_TYPE = "";
+    //Selenium Grid URL
+    public static final String GRID_URL = "";
+    public static final boolean REMOTE = true;
 
 }
--- src/main/java/AutomationPractice/Utils/BrowserFactory.java
+++ src/main/java/AutomationPractice/Utils/BrowserFactory.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BrowserFactory {
@@ -15,18 +19,42 @@ public class BrowserFactory {
 
         String basePath = "./src/main/resources/";
 
-        if (AutomationConstants.BROWSER_TYPE.equalsIgnoreCase("Firefox")) {
-            System.setProperty("webdriver.gecko.driver",  basePath+"geckodriver.exe");
-            driver = new FirefoxDriver();
+        if(AutomationConstants.REMOTE == true){
+
+            DesiredCapabilities caps = DesiredCapabilities.firefox();
+            caps.setCapability("platform", "Windows 7");
+            caps.setCapability("version", "67.0");
+
+//            DesiredCapabilities caps = DesiredCapabilities.edge();
+//            caps.setCapability("platform", "Windows 10");
+//            caps.setCapability("version", "18.17763");
+
+//            DesiredCapabilities caps = DesiredCapabilities.chrome();
+//            caps.setCapability("platform", "Windows 8");
+//            caps.setCapability("version", "58.0");
+
+            try {
+                driver = new RemoteWebDriver(new URL(AutomationConstants.GRID_URL),caps);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
-        else if (AutomationConstants.BROWSER_TYPE.equalsIgnoreCase("Chrome")) {
-            System.setProperty("webdriver.chrome.driver", basePath+"chromedriver.exe");
-            driver = new ChromeDriver();
+        else{
+
+            if (AutomationConstants.BROWSER_TYPE.equalsIgnoreCase("Firefox")) {
+                System.setProperty("webdriver.gecko.driver",  basePath+"geckodriver.exe");
+                driver = new FirefoxDriver();
+            }
+            else if (AutomationConstants.BROWSER_TYPE.equalsIgnoreCase("Chrome")) {
+                System.setProperty("webdriver.chrome.driver", basePath+"chromedriver.exe");
+                driver = new ChromeDriver();
+            }
+            else if (AutomationConstants.BROWSER_TYPE.equalsIgnoreCase("IE")) {
+                System.setProperty("webdriver.ie.driver", basePath+"IEDriverServer.exe");
+                driver = new InternetExplorerDriver();
+            }
         }
-        else if (AutomationConstants.BROWSER_TYPE.equalsIgnoreCase("IE")) {
-            System.setProperty("webdriver.ie.driver", basePath+"IEDriverServer.exe");
-            driver = new InternetExplorerDriver();
-        }
+
         driver.get(AutomationConstants.baseURL);
         driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
     }
