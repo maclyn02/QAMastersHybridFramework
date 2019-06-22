9edd31ed215bdcbddeafe2c16496cee82e39c05d
maclyn02 <mac.afonso@yahoo.com>
2019-06-22 16:36:59 +0100

    First commit

--- src/main/java/AutomationPractice/Utils/AutomationConstants.java
+++ src/main/java/AutomationPractice/Utils/AutomationConstants.java
@@ -5,9 +5,6 @@ public class AutomationConstants {
     //Global Test Data
     public static final String baseURL = "http://automationpractice.com";
     //Browser settings
-    public static final String BROWSER_TYPE = "";
-    //Selenium Grid URL
-    public static final String GRID_URL = "";
-    public static final boolean REMOTE = true;
+    public static final String BROWSER_TYPE = "Chrome";
 
 }
